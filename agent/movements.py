8ce6aa772c50f34f13f3ae3f14fd88463ea05abb
William Spongberg <william.spongberg@outlook.com>
2024-04-17 15:19:03 +1000

    updated movement code to work with current code

--- agent/movements.py
+++ agent/movements.py
@@ -1,7 +1,7 @@
-from referee.game import PlayerColor, Coord, PlaceAction
-from .heuristics import coord_distance_to_goal_line
+from referee.game import PlayerColor, Coord, PlaceAction, Direction
+from referee.game.board import Board, CellState
 
-def get_valid_moves(board: dict[Coord, PlayerColor], tetronimos: list[PlaceAction], coord: Coord) -> list[PlaceAction]:
+def get_valid_moves(state: dict[Coord, CellState], tetronimos: list[PlaceAction], coord: Coord) -> list[PlaceAction]:
     """
     Get valid PlaceActions from a given coordinate.
     """
@@ -9,33 +9,37 @@ def get_valid_moves(board: dict[Coord, PlayerColor], tetronimos: list[PlaceActio
     
     # for each piece, check if valid, if valid, add to list of possible moves
     for move in get_moves(coord, tetronimos):
-        if is_valid(board, move):
+        if is_valid(state, move):
             valid_moves.append(move)
+        # else:
+        #     print(f"invalid move: {move}")
 
     #print(valid_moves)
     return valid_moves
 
-def get_valid_adjacents_all_over_the_board(board: dict[Coord, PlayerColor], goal: Coord) -> list[Coord]:
+def get_valid_coords(state: dict[Coord, CellState], player_colour: PlayerColor) -> list[Coord]:
     """
     Get valid adjacent coordinates from all over the board.
     """
     valid_adjacents = []
-    for coord in board:
-        if (coord is not None) and (board.get(coord, None) == PlayerColor.RED):
-            directions = [coord.up(), coord.down(), coord.left(), coord.right()]
-            adjacents = [Coord(dir.r, dir.c) for dir in directions]
+    for coord in state.keys():
+        if state[coord].player == player_colour:
+            adjacents = [coord + dir for dir in Direction]
             for adjacent in adjacents:
-                if not board.get(adjacent, None): # if adjacent is empty
+                if state[adjacent].player is None: # if adjacent is empty
                     valid_adjacents.append(adjacent)
-    valid_adjacents.sort(key=lambda x: coord_distance_to_goal_line(board, goal, x))
+    if not valid_adjacents:
+        print("no valid adjacents found, returning all coords")
+        for coord in state.keys():
+            valid_adjacents.append(coord)
     return valid_adjacents
 
-def is_valid(board: dict[Coord, PlayerColor], piece: PlaceAction) -> bool:
+def is_valid(state: dict[Coord, CellState], piece: PlaceAction) -> bool:
     """
     Check if the piece can be placed on the board.
     """
     for coord in piece.coords:
-        if board.get(coord, None):
+        if state[coord].player is not None:
             return False
     return True
 
