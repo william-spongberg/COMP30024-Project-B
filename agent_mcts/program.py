4c1211bc9bffbd31e01e1fbbf5c0c7e41e56c433
William Spongberg <william.spongberg@outlook.com>
2024-04-30 18:56:48 +1000

    Initial barebones commit of MCTS

---
+++ agent_mcts/program.py
@@ -0,0 +1,229 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part B: Game Playing self
+
+import random
+
+# import tensorflow as tf
+from cmath import log
+from collections import defaultdict
+from agent_random import tetronimos
+from agent_random.movements import get_valid_coords, get_valid_moves
+from agent_random.tetronimos import get_tetronimos
+from referee.game import (
+    PlayerColor,
+    Action,
+    PlaceAction,
+    Coord,
+    actions,
+)
+from referee.game.board import Board, CellState
+
+# TODO: redesign Board data structure to be more efficient
+
+
+class Agent:
+    """
+    This class is the "entry point" for your self, providing an interface to
+    respond to various Tetress game events.
+    """
+
+    # attributes
+    game_board: Board  # to keep track of game
+    game_state: dict[Coord, CellState]  # to try different moves
+    tetronimos: list[PlaceAction]  # list of all possible tetronimos
+    opponent: PlayerColor  # to keep track of opponent
+    sim_logs: list[str]  # to keep track of simulation results
+    sim_commentary: list[str]  # to keep track of simulation commentary
+    sim_game_num: int  # to keep track of simulation number
+
+    def __init__(self, color: PlayerColor, **referee: dict):
+        """
+        This constructor method runs when the referee instantiates the self.
+        Any setup and/or precomputation should be done here.
+        """
+        self.game_board = Board()
+        self.game_state = self.game_board._state
+        self.tetronimos = get_tetronimos(Coord(0, 0))
+        self._color = color
+        self.name = "self_Next " + self._color.name
+        self.sim_logs = []
+        self.sim_commentary = []
+        self.sim_game_num = 0
+
+        # test tetronimos
+        with open("tetronimos_test.txt", "w") as f:
+            for tetronimo in get_tetronimos(Coord(5, 5)):
+                board = Board()
+                board.apply_action(tetronimo)
+                print(board.render(), file=f)
+
+        # announce self
+        print(f"{self.name} *init*: {self._color}")
+
+        # set opponent colour
+        match color:
+            case PlayerColor.RED:
+                self.opponent = PlayerColor.BLUE
+            case PlayerColor.BLUE:
+                self.opponent = PlayerColor.RED
+
+    def action(self, **referee: dict) -> Action:
+        """
+        This method is called by the referee each time it is the self's turn
+        to take an action. It must always return an action object.
+        """
+        root = MCTSNode(self.game_board)
+        action = root.best_action()
+
+        if action:
+            return action
+        else:
+            return self.get_random_move()
+
+    def update(self, color: PlayerColor, action: Action, **referee: dict):
+        """
+        This method is called by the referee after an agent has taken their
+        turn. You should use it to update the agent's internal game state.
+        """
+
+        self.game_board.apply_action(action)
+        self.game_state = self.game_board._state
+
+    def get_random_move(self) -> PlaceAction:
+        coords = get_valid_coords(self.game_state, self._color)
+        coord: Coord = random.choice(coords)
+        coords.remove(coord)
+
+        # try all available coords
+        while get_valid_moves(self.game_state, self.tetronimos, coord) == []:
+            if coords:
+                coord = random.choice(coords)
+                coords.remove(coord)
+            else:
+                break
+        # if no valid moves available
+        if get_valid_moves(self.game_state, self.tetronimos, coord) == []:
+            return PlaceAction(Coord(0, 0), Coord(0, 0), Coord(0, 0), Coord(0, 0))
+        return random.choice(get_valid_moves(self.game_state, self.tetronimos, coord))
+
+
+class MCTSNode:
+    def __init__(self, board, parent=None, parent_action=None):
+        self.board: Board = board
+        self.state: dict[Coord, CellState] = board._state
+        self.parent = parent
+        self.parent_action = parent_action
+        self.children = []
+
+        self._num_visits = 0
+        self._actions: list[PlaceAction] = self.get_actions()
+        self._results = defaultdict(int)
+        self._results[1] = 0  # win
+        self._results[-1] = 0  # loss
+
+    def get_actions(self):
+        coords: list[Coord] = get_valid_coords(self.state, self.board.turn_color)
+        tetronimos: list[PlaceAction] = get_tetronimos(Coord(0, 0))
+        actions: list[PlaceAction] = []
+        for coord in coords:
+            actions.extend(get_valid_moves(self.state, tetronimos, coord))
+        return actions
+
+    def num_visits(self):
+        return self._num_visits
+
+    def expand(self):
+        action = self._actions.pop()
+        print(action)
+        # TODO: fix get_actions not returning valid actions
+        self.board.apply_action(action)
+        next_state = self.board._state
+        child_node = MCTSNode(next_state, self, action)
+
+        self.children.append(child_node)
+        return child_node
+
+    def is_terminal_node(self):
+        return self.board.game_over
+
+    def is_fully_expanded(self):
+        return len(self._actions) == 0
+
+    def rollout(self):
+        current_board = self.board
+        while not current_board.game_over:
+            # light playout policy
+            # TODO: implement heuristic-driven playout policy
+            action = self.get_random_move()
+            current_board.apply_action(action)
+        return current_board.winner_color
+
+    def backpropagate(self, result):
+        self._num_visits += 1
+        self._results[result] += 1
+        if self.parent:
+            self.parent.backpropagate(result)
+
+    def best_child(self, c_param=1.4):
+        best_score = -1
+        best_child = None
+        for child in self.children:
+            exploit = child._results[1] / child._num_visits
+            explore = c_param * (2 * log(self._num_visits) / child._num_visits) ** 0.5
+            score = exploit + explore
+            if score > best_score:
+                best_score = score
+                best_child = child
+        return best_child
+
+    def _tree_policy(self):
+        current_node: MCTSNode | None = self
+        # select nodes to expand
+        while current_node and current_node.is_terminal_node():
+            if not current_node.is_fully_expanded():
+                return current_node.expand()
+            else:
+                current_node = current_node.best_child()
+        return current_node
+
+    def best_action(self, sim_no=100) -> PlaceAction | None:
+        # run MCTS
+        for _ in range(sim_no):
+            # expansion
+            v: MCTSNode | None = self._tree_policy()
+            if not v:
+                print("ERROR: No tree policy node found")
+                return None
+            # simulation
+            reward = v.rollout()
+            # backpropagation
+            v.backpropagate(reward)
+
+        # return best action
+        best_child = self.best_child(c_param=0.0)
+        if best_child:
+            return best_child._parent_action
+
+        # if no best child, print error + return None
+        print("ERROR: No best child found")
+        return None
+
+    def get_random_move(self) -> PlaceAction:
+        coord = random.choice(get_valid_coords(self.state, self.board.turn_color))
+        action = PlaceAction(
+            Coord(0, 0), Coord(0, 0), Coord(0, 0), Coord(0, 0)
+        )  # default
+        tetronimos = get_tetronimos(Coord(0, 0))
+
+        # if no valid moves, pick a new coord
+        if get_valid_moves(self.state, tetronimos, coord) == []:
+            # try all available coords
+            for coord in get_valid_coords(self.state, self.board.turn_color):
+                if get_valid_moves(self.state, tetronimos, coord) != []:
+                    action = random.choice(
+                        get_valid_moves(self.state, tetronimos, coord)
+                    )
+                    break
+        else:
+            action = random.choice(get_valid_moves(self.state, tetronimos, coord))
+        return action
