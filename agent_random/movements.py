5e8c06fd5b72e070501829eda75f636bf3492396
William Spongberg <william.spongberg@outlook.com>
2024-05-01 22:52:55 +1000

    Converted to list comprehension for efficiency

--- agent_random/movements.py
+++ agent_random/movements.py
@@ -1,38 +1,36 @@
 from referee.game import PlayerColor, Coord, PlaceAction, Direction
 from referee.game.board import Board, CellState
 
-def valid_moves(state: dict[Coord, CellState], tetronimos: list[PlaceAction], coord: Coord) -> list[PlaceAction]:
+
+def valid_moves(
+    state: dict[Coord, CellState], tetronimos: list[PlaceAction], coord: Coord
+) -> list[PlaceAction]:
     """
     Get valid PlaceActions from a given coordinate.
     """
-    valid_moves = []
-    
-    # for each piece, check if valid, if valid, add to list of possible moves
-    for move in get_moves(coord, tetronimos):
-        if is_valid(state, move):
-            valid_moves.append(move)
-        # else:
-        #     print(f"invalid move: {move}")
+    return [move for move in get_moves(coord, tetronimos) if is_valid(state, move)]
 
-    #print(valid_moves)
-    return valid_moves
 
-def valid_coords(state: dict[Coord, CellState], player_colour: PlayerColor) -> list[Coord]:
+def valid_coords(
+    state: dict[Coord, CellState], player_colour: PlayerColor
+) -> list[Coord]:
     """
     Get valid adjacent coordinates from all over the board.
     """
-    valid_adjacents = []
-    for coord in state.keys():
-        if state[coord].player == player_colour:
-            adjacents = [coord + dir for dir in Direction]
-            for adjacent in adjacents:
-                if state[adjacent].player is None: # if adjacent is empty
-                    valid_adjacents.append(adjacent)
-    if not valid_adjacents:
-        #print("no valid adjacents found, returning all coords")
-        for coord in state.keys():
-            valid_adjacents.append(coord)
-    return valid_adjacents
+    
+    # if dict does not contain any player colour coords, return all coords
+    if not any([cell.player == player_colour for cell in state.values()]):
+        return list(state.keys())
+    
+    # else if dict contains a player colour coord, return all adjacent coords
+    return [
+        adjacent
+        for coord in state.keys()
+        if state[coord].player == player_colour
+        for adjacent in [coord + dir for dir in Direction]
+        if state[adjacent].player is None
+    ]
+
 
 def is_valid(state: dict[Coord, CellState], piece: PlaceAction) -> bool:
     """
@@ -43,13 +41,12 @@ def is_valid(state: dict[Coord, CellState], piece: PlaceAction) -> bool:
             return False
     return True
 
+
 def get_moves(coord: Coord, tetronimos: list[PlaceAction]) -> list[PlaceAction]:
     """
-    get all possible tetronimo moves from a given coordinate
+    Get all possible tetronimo moves from a given coordinate
     """
-    list_of_moves = []
-    for tetronimo in tetronimos:
-        move = [coord + Coord(x, y) for x, y in list(tetronimo.coords)]
-        move = PlaceAction(*move)
-        list_of_moves.append(move)
-    return list_of_moves
+    return [
+        PlaceAction(*[coord + Coord(x, y) for x, y in list(tetronimo.coords)])
+        for tetronimo in tetronimos
+    ]
