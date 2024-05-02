65b2f2fc27a48e3632fea9f8a47cfcc3a254e93c
William Spongberg <william.spongberg@outlook.com>
2024-05-02 14:52:19 +1000

    Formatted tetronimos.py

--- agent_random/tetronimos.py
+++ agent_random/tetronimos.py
@@ -3,22 +3,22 @@ from referee.game.pieces import create_piece, PieceType
 
 BOARD_N = 11
 
+
 def make_tetronimos(coord: Coord) -> list[PlaceAction]:
     """
     Get all possible tetronimos as PlaceActions.
     """
-    
+
     tetronimos = []
-    
+
     # get all possible orientations of a piece
     for piece_type in PieceType:
         tetronimos.append(create_piece(piece_type, coord))
-            
+
     # remove duplicates
     tetronimos = list(set(tetronimos))
-    
+
     # convert all elements to PlaceAction
     for i, tetronimo in enumerate(tetronimos):
         tetronimos[i] = PlaceAction(*tetronimo.coords)
-    
     return tetronimos
