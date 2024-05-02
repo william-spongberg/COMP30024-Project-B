24ec4de004f0092d4aec2f28fb60621260a26df8
William Spongberg <william.spongberg@outlook.com>
2024-05-02 14:50:49 +1000

    Updated random program formatting, removed tetronimo creation

--- agent_random/program.py
+++ agent_random/program.py
@@ -13,10 +13,9 @@ class Agent:
 
     # attributes
     board: Board  # state of game
-    colour: PlayerColor  # agent colour
+    color: PlayerColor  # agent colour
     name: str  # agent name
     opponent: PlayerColor  # agent opponent
-    tetronimos: list[PlaceAction]  # list of all possible tetronimos
 
     def __init__(self, color: PlayerColor, **referee: dict):
         self.init(color)
@@ -29,9 +28,8 @@ class Agent:
 
     def init(self, color: PlayerColor):
         self.board = Board()
-        self.colour = color
-        self.name = "Agent_Random " + self.colour.name
-        self.tetronimos = make_tetronimos(Coord(0, 0))
+        self.color = color
+        self.name = "Agent_Random " + self.color.name
 
         match color:
             case PlayerColor.RED:
@@ -39,38 +37,39 @@ class Agent:
             case PlayerColor.BLUE:
                 self.opponent = PlayerColor.RED
 
-        print(f"{self.name} *initiated*: {self.colour}")
+        print(f"{self.name} *initiated*: {self.color}")
 
     def get_random_move(self) -> PlaceAction:
-        coords = valid_coords(self.board._state, self.colour)
+        coords = valid_coords(self.board._state, self.color)
         coord: Coord = random.choice(coords)
         coords.remove(coord)
 
         # try all available coords
-        while valid_moves(self.board._state, self.tetronimos, coord) == []:
+        while not valid_moves(self.board._state, coord):
             if coords:
                 coord = random.choice(coords)
                 coords.remove(coord)
             else:
                 break
         # if no valid moves available
-        if valid_moves(self.board._state, self.tetronimos, coord) == []:
+        if not valid_moves(self.board._state, coord):
             return PlaceAction(Coord(0, 0), Coord(0, 0), Coord(0, 0), Coord(0, 0))
 
         # return random move
-        return random.choice(valid_moves(self.board._state, self.tetronimos, coord))
+        return random.choice(valid_moves(self.board._state, coord))
+
 
 class AgentRandom:
     # wrap Agent class
     def __init__(self, color: PlayerColor, **referee: dict):
         self.agent = Agent(color)
-    
+
     def action(self, **referee: dict) -> Action:
         return self.agent.action()
-    
+
     def update(self, color: PlayerColor, action: Action, **referee: dict):
         self.agent.update(color, action)
-        
+
     @property
-    def colour(self):
-        return self.agent.colour
+    def color(self):
+        return self.agent.color
