d924b7850683d954d2abbab8c2f90f4dc98cb1ac
XinMoZ <GaoyongleZ@student.unimelb.edu.au>
2024-05-04 17:05:24 +1000

    Fix tetrominoes doesn't find all moves

--- helpers/tetrominoes.py
+++ helpers/tetrominoes.py
@@ -1,6 +1,6 @@
 from referee.game import PlaceAction, Coord
 from referee.game.board import Board
-from referee.game.pieces import create_piece, PieceType
+from referee.game.pieces import Piece, create_piece, PieceType
 
 BOARD_N = 11
 
@@ -15,23 +15,22 @@ def make_tetrominoes(coord: Coord) -> list[PlaceAction]:
 
     # get all possible orientations of a piece
     for piece_type in PieceType:
-        tetrominoes.append(create_piece(piece_type, coord))
+        tetrominoes.append(create_piece(piece_type, Coord(0, 0)))
 
     # remove duplicates
     tetrominoes = list(set(tetrominoes))
+    
+    for tetromino in tetrominoes:
+        # choose different starting origins
+        for origin in tetromino.coords:
+            moved_tetrominoes.append(Piece([coord + Coord(x, y) - origin for x, y in tetromino.coords]))
+    
 
     # convert all elements to PlaceAction
-    for i, tetromino in enumerate(tetrominoes):
-        tetrominoes[i] = PlaceAction(*tetromino.coords)
+    for i, moved_tetromino in enumerate(moved_tetrominoes):
+        moved_tetrominoes[i] = PlaceAction(*moved_tetromino.coords)
     
-    # # get all possible moves of a piece
-    # for tetromino in tetrominoes:
-    #     for coord in list(tetromino.coords):
-    # TODO: copy tetronimo to have all 4 coords be the start coord
-            
-        
-    
-    return tetrominoes
+    return moved_tetrominoes
 
 
 def test_tetronimoes():
