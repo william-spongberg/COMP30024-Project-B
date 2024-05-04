5af8e6222a02a22633e240004c4b7933026492e0
William Spongberg <william.spongberg@outlook.com>
2024-05-04 14:31:32 +1000

    Added debug code in agent_random program.py for valid_moves

--- agent_random/program.py
+++ agent_random/program.py
@@ -1,9 +1,11 @@
 # COMP30024 Artificial Intelligence, Semester 1 2024
 # Project Part B: Game Playing Agent
 
+import copy
 import random
 
-from agent_random.movements import valid_moves, valid_coords
+from helpers.sim_board import SimBoard
+from helpers.movements import valid_moves, valid_coords
 from referee.game import PlayerColor, Action, PlaceAction, Coord
 from referee.game.board import Board
 
@@ -11,7 +13,7 @@ from referee.game.board import Board
 class Agent:
 
     # attributes
-    board: Board  # state of game
+    board: SimBoard  # state of game
     color: PlayerColor  # agent colour
     name: str  # agent name
     opponent: PlayerColor  # agent opponent
@@ -26,36 +28,42 @@ class Agent:
         self.board.apply_action(action)
 
     def init(self, color: PlayerColor):
-        self.board = Board()
+        self.board = SimBoard(init_color=color)
         self.color = color
         self.name = "Agent_Random " + self.color.name
-
-        match color:
-            case PlayerColor.RED:
-                self.opponent = PlayerColor.BLUE
-            case PlayerColor.BLUE:
-                self.opponent = PlayerColor.RED
+        self.opponent = self.color.opponent
 
         print(f"{self.name} *initiated*: {self.color}")
 
     def get_random_move(self) -> PlaceAction:
-        coords = valid_coords(self.board._state, self.color)
+        coords = valid_coords(self.board.state, self.color)
         coord: Coord = random.choice(coords)
         coords.remove(coord)
 
         # try all available coords
-        while not valid_moves(self.board._state, coord):
+        while not valid_moves(self.board.state, coord):
             if coords:
                 coord = random.choice(coords)
                 coords.remove(coord)
             else:
                 break
         # if no valid moves available
-        if not valid_moves(self.board._state, coord):
+        if not valid_moves(self.board.state, coord):
             return PlaceAction(Coord(0, 0), Coord(0, 0), Coord(0, 0), Coord(0, 0))
+        
+        # *DEBUGGING* valid moves not working correctly, invalid moves being generated
+        print(f"valid moves at coord {coord}:")
+        for move in valid_moves(self.board.state, coord): # type: ignore
+            print(move)
+            temp_board = copy.deepcopy(self.board)
+            temp_board.apply_action(move)
+            #print(temp_board.render(True))
+        print("board colour: ", self.board.turn_color)
+        print(f"generated {len(valid_moves(self.board.state, coord))} valid moves at {coord}") # type: ignore
+            
 
         # return random move
-        return random.choice(valid_moves(self.board._state, coord))
+        return random.choice(valid_moves(self.board.state, coord)) # type: ignore
 
 
 class AgentRandom:
