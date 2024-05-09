0999ef9ebe5485408b1532c053e33fa7c6c24f2d
William Spongberg <william.spongberg@outlook.com>
2024-05-09 14:52:20 +1000

    Updated test.py to use SimBoard, now runs simulation 10 times to find more reliable averages

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import cProfile
+import pstats
 import sys
 
 from agent_mcts.program import AgentMCTS
@@ -49,32 +50,32 @@ def check_agent_state(agent_blue, game_state):
 def play_game():
     # test_tetronimoes()
     agent_red, agent_blue = get_agents()
-    game_state: Board = Board()  # SimBoard = SimBoard()
+    game_state: SimBoard = SimBoard()
 
     # play game until over
     while not game_state.game_over:
         if game_state.turn_color == agent_red.color:
             # agent A turn
             move = agent_red.action()
-            print(f"{agent_red.name} placed", move)
+            # print(f"{agent_red.name} placed", move)
             # print(game_state.render(True))
         else:
             # agent B turn
             move = agent_blue.action()
-            print(f"{agent_blue.name} placed", move)
+            # print(f"{agent_blue.name} placed", move)
             # print(game_state.render(True))
 
         # apply move to game state
         game_state.apply_action(move)
-        print(game_state.render(True))
+        # print(game_state.render(True))
 
         # update agents
         agent_red.update(game_state.turn_color, move)
         agent_blue.update(game_state.turn_color, move)
-            
+
         # *debug*
-        check_agent_state(agent_red, game_state)
-        check_agent_state(agent_blue, game_state)
+        # check_agent_state(agent_red, game_state)
+        # check_agent_state(agent_blue, game_state)
 
     # print final game state
     print("final game state:")
@@ -82,5 +83,16 @@ def play_game():
     print(game_state.turn_color.opponent, " wins")
 
 
-# profile the game
-play_game()
+def play_game_multiple_times():
+    for _ in range(10):
+        play_game()
+
+
+# play_game()
+# cProfile.run("play_game()", "test.prof")
+
+
+cProfile.run("play_game_multiple_times()", "test.prof")
+p = pstats.Stats("test.prof")
+p.strip_dirs().sort_stats("cumulative").print_stats(10)
+p.strip_dirs().sort_stats("time").print_stats(10)
